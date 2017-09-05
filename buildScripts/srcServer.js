a30bc143712089a085697eb6a89566237e620db3
AJENT47 <ajnt47@gmail.com>
2017-09-05 23:11:35 +0530

    Bundling using webpack 2

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,8 +2,17 @@ import express from 'express'; // webserver (web framework)
 import path from 'path';  // provides utilities for working with file and directory paths
 import open from 'open';  // Open a file or url in the user's preferred application
 
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
